7942c1afb83973f71f690c04ca4567447b53b50b
Mridul Tiwari <mridultiwari522@gmail.com>
2024-10-06 11:24:15 +0000

    variables

---
+++ 01_Basic/01_Variable.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accEmail = "abc@google.com"
+var accPassword = "123321"
+
+// lets do change in variables
+
+/* accountId = 123 */
+
+accEmail = "cba@google.com"
+
+console.table([accountId ,accEmail ,accPassword])
